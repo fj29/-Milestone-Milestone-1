f6344d66686d5956b4564c20a57b2c4984a2a2fc
fj29 <125627007+fj29@users.noreply.github.com>
2024-04-02 05:35:22 +0500

    Milestone2

--- 114/ChatRoom/Project/Server/Room.java
+++ 114/ChatRoom/Project/Server/Room.java
@@ -8,6 +8,8 @@ import java.util.logging.Logger;
 import Project.Common.Constants;
 
 public class Room implements AutoCloseable {
+    private static String flip;
+    private static String roll;
     // protected static Server server;// used to refer to accessible server
     // functions
     private String name;
@@ -16,6 +18,8 @@ public class Room implements AutoCloseable {
     private boolean isRunning = false;
     // Commands
     private final static String COMMAND_TRIGGER = "/";
+    private static final String FLIP = flip;
+    private static final String ROLL = roll;
     // private final static String CREATE_ROOM = "createroom";
     // private final static String JOIN_ROOM = "joinroom";
     // private final static String DISCONNECT = "disconnect";
@@ -114,6 +118,45 @@ public class Room implements AutoCloseable {
                      * Room.disconnectClient(client, this);
                      * break;
                      */
+                    
+                // ucid: fj28 
+                // date: 4/1/24     
+                    case "FLIP":
+                    int coin = (int) (Math.random() * 2); 
+                    if (coin == 0) {
+                    sendMessage(null, String.format("<b>%s did a coin flip and landed on Heads</b>", client.getClientName()));
+                     } else if (coin == 1) {
+                    sendMessage(null, String.format("<b>%s did a coin flip and landed on Tails</b>", client.getClientName()));
+                     }
+                    break;
+
+                    case "ROLL":
+                    int total = 0;
+                    try {
+                    String[] rollParts = message.trim().split(" ");
+                    if (rollParts.length >= 2 && rollParts[1].contains("d")) {
+                    String[] rollParams = rollParts[1].split("d");
+                    int numberOfDice = Integer.parseInt(rollParams[0]);
+                    int sidesOfDice = Integer.parseInt(rollParams[1]);
+                    for (int i = 0; i < numberOfDice; i++) {
+                    int rollDice = (int) (Math.random() * sidesOfDice) + 1;
+                    total += rollDice;
+            }
+                    sendMessage(null, String.format("<b>%s did a roll of <u>%sd%s</u> and got a total roll of >%s<</b>", client.getClientName(), numberOfDice, sidesOfDice, total));
+                    } else if (rollParts.length >= 2) {
+                    int value = Integer.parseInt(rollParts[1]);
+                    int singleDiceRoll = (int) (Math.random() * value) + 1;
+                    sendMessage(null, String.format("<b>%s did a >roll< with a range of <u>1-%s</u> and the result is \">%s</b>", client.getClientName(), value, singleDiceRoll));
+                    } else {
+                    client.sendMessage(-1, "<b><font color=\"red\">invalid input</font></b>");
+                    }
+                    } catch (NumberFormatException e) {
+                    client.sendMessage(-1, "<b><font color=\"red\">invalid input</font></b>");
+                    }
+                    break;
+
+
+                     
                     default:
                         wasCommand = false;
                         break;
@@ -174,6 +217,107 @@ public class Room implements AutoCloseable {
         if (!isRunning) {
             return;
         }
+
+        //FJ28 4.1.24
+        //Altered message output
+        String startTag = "";
+        String endTag = "";
+        int startIndex = -1;
+        int endIndex = -1;
+        boolean processed = true;
+        //FJ28 4.1.24
+        while(processed){
+            processed = false;
+            startIndex = message.indexOf("*b");
+            endIndex = message.indexOf("b*");
+            //bold
+            if(startIndex > -1 && endIndex > -1 && endIndex > startIndex+2){
+                processed = true;
+                startTag = "<b>";
+                endTag = "</b>";
+                message = message.substring(0, startIndex) + startTag 
+                + message.substring(startIndex+2, endIndex) + endTag 
+                + message.substring(endIndex+2); 
+            }
+            //italic
+            startIndex = message.indexOf("*i");
+            endIndex = message.indexOf("i*");
+            if(startIndex > -1 && endIndex > -1 && endIndex > startIndex+2){
+                processed = true;
+                startTag = "<i>";
+                endTag = "</i>";
+                message = message.substring(0, startIndex) + startTag 
+                + message.substring(startIndex+2, endIndex) + endTag 
+                + message.substring(endIndex+2);
+            }
+            //underline
+            startIndex = message.indexOf("*u");
+            endIndex = message.indexOf("u*");
+            if(startIndex > -1 && endIndex > -1 && endIndex > startIndex+2){
+                processed = true;
+                startTag = "<u>";
+                endTag = "</u>";
+                message = message.substring(0, startIndex) + startTag 
+                + message.substring(startIndex+2, endIndex) + endTag 
+                + message.substring(endIndex+2);
+            }
+            //red
+            startIndex = message.indexOf("#r");
+            endIndex = message.indexOf("r#");
+            if(startIndex > -1 && endIndex > -1 && endIndex > startIndex+2){
+                processed = true;
+                startTag = "<font color=\"red\">";
+                endTag = "</font>";
+                message = message.substring(0, startIndex) + startTag 
+                + message.substring(startIndex+2, endIndex) + endTag 
+                + message.substring(endIndex+2);
+            }
+            //green
+            startIndex = message.indexOf("#g");
+            endIndex = message.indexOf("g#");
+            if(startIndex > -1 && endIndex > -1 && endIndex > startIndex+2){
+                processed = true;
+                startTag = "<font color=\"green\">";
+                endTag = "</font>";
+                message = message.substring(0, startIndex) + startTag 
+                + message.substring(startIndex+2, endIndex) + endTag 
+                + message.substring(endIndex+2);
+            }
+            //blue
+            startIndex = message.indexOf("#b");
+            endIndex = message.indexOf("b#");
+            if(startIndex > -1 && endIndex > -1 && endIndex > startIndex+2){
+                processed = true;
+                startTag = "<font color=\"blue\">";
+                endTag = "</font>";
+                message = message.substring(0, startIndex) + startTag 
+                + message.substring(startIndex+2, endIndex) + endTag 
+                + message.substring(endIndex+2);
+            }
+            
+            //Whisper/Private message
+            startIndex = message.indexOf("@");
+            if (startIndex > -1) {
+                endIndex = message.indexOf(" ", startIndex);
+                if (endIndex == -1) {
+                    endIndex = message.length();
+                }
+                String clientName = message.substring(startIndex + 1, endIndex);
+                for (ServerThread client : clients) {
+                    if (client != sender && client.getClientName().equals(clientName)) {
+                        client.sendMessage(sender.getClientId(), message);
+                        sender.sendMessage(sender.getClientId(), message);
+                        return;
+                    }
+                }
+            }
+
+
+        } 
+        System.out.println(message);
+    
+        //End
+
         info("Sending message to " + clients.size() + " clients");
         if (sender != null && processCommands(message, sender)) {
             // it was a command, don't broadcast
@@ -191,6 +335,7 @@ public class Room implements AutoCloseable {
             }
         }
     }
+	
 
     protected synchronized void sendConnectionStatus(ServerThread sender, boolean isConnected) {
         Iterator<ServerThread> iter = clients.iterator();

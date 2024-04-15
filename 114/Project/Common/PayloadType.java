019bef69b0094358c90a214ba498dc19ad8c6412
fj29 <125627007+fj29@users.noreply.github.com>
2024-04-15 18:42:53 -0500

    Milestone3 Added/Updated & Modified

---
+++ 114/Project/Common/PayloadType.java
@@ -0,0 +1,9 @@
+package Project.Common;
+
+// ucid: fj28 
+// date: 4/1/24
+
+public enum PayloadType {
+    CONNECT, DISCONNECT, MESSAGE, CREATE_ROOM, JOIN_ROOM, LIST_ROOMS, CLIENT_ID, RESET_USER_LIST, SYNC_CLIENT, GET_ROOMS,
+    READY, FLIP, ROLL, MUTE, UNMUTE, 
+}
